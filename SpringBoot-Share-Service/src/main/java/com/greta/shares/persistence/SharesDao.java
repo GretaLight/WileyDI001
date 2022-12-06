dd7711ed27f902c48bcd0530a6d7c44cb55e2e58
GretaLight <110153958+GretaLight@users.noreply.github.com>
2022-12-06 12:25:57 +0000

    Shares-Service

---
+++ SpringBoot-Share-Service/src/main/java/com/greta/shares/persistence/SharesDao.java
@@ -0,0 +1,11 @@
+package com.greta.shares.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.greta.shares.entity.Shares;
+
+@Repository
+public interface SharesDao extends JpaRepository<Shares, Integer> {
+
+}
